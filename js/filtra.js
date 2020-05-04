e8ae22d21e6c10a6378c77a5c678ca14667fa9ee
MateusDonaire <51013142+MateusDonaire@users.noreply.github.com>
2020-05-03 22:09:08 -0300

    Adicionado JS - Filtra (Por dificuldade)

--- js/filtra.js
+++ js/filtra.js
@@ -24,3 +24,30 @@ campoFiltro.addEventListener("input", function(){
     }
 });
 
+var campoFiltro = document.querySelector("#filtrar-tabela-dificuldade");
+
+campoFiltro.addEventListener("input", function(){
+    var questoes = document.querySelectorAll(".questao");
+
+    if (this.value.length > 0){
+        for (var i = 0; i < questoes.length; i++){
+            var questao = questoes[i];
+            var tdDificuldade = questao.querySelector(".info-nivelDificuldade");
+            var dificuldade = tdDificuldade.textContent;
+            var expressao = RegExp(this.value, "i");// Expressão Regular
+
+            if (!expressao.test(dificuldade)){
+                questao.classList.add("invisivel");
+            } else {
+                questao.classList.remove("invisivel");
+            }
+        }
+    } else {
+        for (var i = 0; i < questoes.length; i++) {
+            var questao = questoes[i];
+            questao.classList.remove("invisivel");
+        }
+    }
+});
+
+
